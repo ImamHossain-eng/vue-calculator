64a774e09d5d45e2c024b6b0b9300544be3494e3
Babu Imam <babubpx2762@gmail.com>
2020-08-18 21:29:06 -0700

    Vue initialize

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  <calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator'
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
